45e7d73a19c087339eee73aaa7f6e5593dc9a4cd
sindhuregalla <30748552+sindhuregalla@users.noreply.github.com>
2018-01-06 16:50:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,21 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article1',function(req,res)
+{
+    res.send('article1 is requested and will be served here');
+});
+
+app.get('/article2',function(req,res)
+{
+    res.send('article2 is requested and will be served here');
+});
+
+app.get('/article3',function(req,res)
+{
+    res.send('article3 is requested and will be served here');
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
